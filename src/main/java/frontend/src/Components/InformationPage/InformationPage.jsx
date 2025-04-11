dca612230f5f2f844b92fc26b6ea2aeef2506246
TriForMine <quentin@triformine.dev>
2025-04-11 14:36:50 +0200

    Revert "Replace react-router-dom with react-router and update dependencies"
    This reverts commit b75ac5c3

--- src/main/java/frontend/src/Components/InformationPage/InformationPage.jsx
+++ src/main/java/frontend/src/Components/InformationPage/InformationPage.jsx
@@ -1,4 +1,4 @@
-import {useParams} from 'react-router';
+import {useParams} from 'react-router-dom';
 import './InformationPage.css';
 import React, {useEffect, useState} from 'react';
 import {useTitle} from "../../global/useTitle";
--- src/main/java/frontend/src/global/router.jsx
+++ src/main/java/frontend/src/global/router.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router";
+import {createBrowserRouter} from "react-router-dom";
 import App from "./App";
 import LoginForm from "../Components/LoginForm/LoginForm";
 import MainLayout from "./MainLayout";
