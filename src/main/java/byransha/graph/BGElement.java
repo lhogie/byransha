fbc8e7606b42612428568d3ab9dbfda33cfee148
Maxime <jinjamb09@gmail.com>
2025-04-29 16:12:02 +0200

    closes #22 Big node for current + intro to change color depending on the view

--- src/main/java/byransha/graph/BGElement.java
+++ src/main/java/byransha/graph/BGElement.java
@@ -7,7 +7,7 @@ public class BGElement {
 	public String color = color(Color.gray);
 	public String style;
 
-	private String color(Color c) {
+	public static String color(Color c) {
 		return "rgb(" + c.getRed() + ", " + c.getGreen() + ", " + c.getBlue() + ")";
 	}
 }
