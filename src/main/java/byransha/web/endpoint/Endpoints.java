47d0233a86e892e801d617d35426813c026d1e8f
Kazenoyama <kishan.turpin@gmail.com>
2025-07-08 15:14:49 +0200

    Enhance endpoint execution logic to filter by type and include type information in response

--- src/main/java/byransha/web/endpoint/Endpoints.java
+++ src/main/java/byransha/web/endpoint/Endpoints.java
@@ -1,5 +1,6 @@
 package byransha.web.endpoint;
 
+import byransha.web.*;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.BooleanNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -9,9 +10,6 @@ import com.sun.net.httpserver.HttpsExchange;
 import byransha.BBGraph;
 import byransha.BNode;
 import byransha.User;
-import byransha.web.EndpointJsonResponse;
-import byransha.web.NodeEndpoint;
-import byransha.web.WebServer;
 
 public class Endpoints extends NodeEndpoint<BNode> {
 
@@ -31,15 +29,30 @@ public class Endpoints extends NodeEndpoint<BNode> {
 	@Override
 	public EndpointJsonResponse exec(ObjectNode in, User user, WebServer webServer, HttpsExchange http, BNode n) {
 		var data = new ArrayNode(null);
-		graph.findAll(NodeEndpoint.class, e -> true).stream().filter(e -> e.canExec(user)).forEach(e -> {
-			var nn = new ObjectNode(null);
-			nn.set("name", new TextNode(e.name()));
-			nn.set("implementation_class", new TextNode(e.getClass().getName()));
-			nn.set("endpoint_target_type", new TextNode(e.getTargetNodeType().getName()));
-			nn.set("applicable_to_current_node", BooleanNode.valueOf(n.matches(e)));
-			nn.set("description", new TextNode(e.whatIsThis()));
-			data.add(nn);
-		});
+
+		graph.findAll(NodeEndpoint.class, e -> true).stream()
+				.filter(e -> e.canExec(user))
+				.filter(e -> {
+					if (in.has("type")) {
+						try {
+							Class<?> clazz = Class.forName(in.get("type").asText());
+							return clazz.isInstance(e);
+						} catch (ClassNotFoundException ex) {
+							return false;
+						}
+					}
+					return true;
+				})
+				.forEach(e -> {
+					var nn = new ObjectNode(null);
+					nn.set("name", new TextNode(e.name()));
+					nn.set("implementation_class", new TextNode(e.getClass().getName()));
+					nn.set("endpoint_target_type", new TextNode(e.getTargetNodeType().getName()));
+					nn.set("applicable_to_current_node", BooleanNode.valueOf(n.matches(e)));
+					nn.set("description", new TextNode(e.whatIsThis()));
+					nn.set("type", new TextNode(e.type().toString()));
+					data.add(nn);
+				});
 
 		return new EndpointJsonResponse(data, this);
 	}
