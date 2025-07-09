958ac398e5b6db4f597bd1c80acdbabe1a7a4e8a
TriForMine <quentin@triformine.dev>
2025-07-09 09:12:43 +0200

    Remove field caching and replace `JsonNodeFactory` with direct node creation
    - Eliminated unused field caching mechanism in `ClassAttributeField`.
    - Replaced `JsonNodeFactory` usage with direct creation of respective node types.
    - Improved readability by streamlining node and validation handling logic.

--- src/main/java/byransha/web/endpoint/ClassAttributeField.java
+++ src/main/java/byransha/web/endpoint/ClassAttributeField.java
@@ -14,18 +14,9 @@ import com.sun.net.httpserver.HttpsExchange;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class ClassAttributeField extends NodeEndpoint<BNode> implements View {
-    private static final Map<String, Field> fieldCache = new ConcurrentHashMap<>();
-    
-    private static final JsonNodeFactory nodeFactory = JsonNodeFactory.instance;
-    
-    private static final NullNode NULL_NODE = NullNode.getInstance();
-    private static final BooleanNode TRUE_NODE = BooleanNode.TRUE;
-    private static final BooleanNode FALSE_NODE = BooleanNode.FALSE;
 
     @Override
     public String whatItDoes() {
@@ -41,27 +32,17 @@ public class ClassAttributeField extends NodeEndpoint<BNode> implements View {
     }
 
     private Field findField(Class<?> clazz, String name) {
-        String cacheKey = clazz.getName() + "#" + name;
-        
-        Field cachedField = fieldCache.get(cacheKey);
-        if (cachedField != null) {
-            return cachedField;
-        }
-        
         Class<?> current = clazz;
         while (current != null && current != Object.class) {
             try {
                 Field field = current.getDeclaredField(name);
                 field.setAccessible(true);
-                fieldCache.put(cacheKey, field);
                 return field;
             } catch (NoSuchFieldException e) {
                 // Not in current class, try superclass
             }
             current = current.getSuperclass();
         }
-        
-        fieldCache.put(cacheKey, null);
         return null;
     }
 
@@ -69,77 +50,76 @@ public class ClassAttributeField extends NodeEndpoint<BNode> implements View {
     @Override
     public EndpointJsonResponse exec(ObjectNode in, User user, WebServer webServer, HttpsExchange exchange, BNode node)
             throws Throwable {
-        var a = nodeFactory.arrayNode();
+        var a = new ArrayNode(null);
 
-        var currentNodeInformation = nodeFactory.objectNode();
-        var currentNodeInfo = nodeFactory.objectNode();
-        currentNodeInfo.set("id", nodeFactory.numberNode(node.id()));
-        currentNodeInfo.set("name", nodeFactory.textNode(node.prettyName()));
-        currentNodeInfo.set("type", nodeFactory.textNode(node.getClass().getSimpleName()));
+        var currentNodeInformation = new ObjectNode(null);
+        var currentNodeInfo = new ObjectNode(null);
+        currentNodeInfo.set("id", new IntNode(node.id()));
+        currentNodeInfo.set("name", new TextNode(node.prettyName()));
+        currentNodeInfo.set("type", new TextNode(node.getClass().getSimpleName()));
         if (node instanceof BusinessNode businessNode) {
-            currentNodeInfo.set("isValid", businessNode.isValid() ? TRUE_NODE : FALSE_NODE);
+            currentNodeInfo.set("isValid", BooleanNode.valueOf(businessNode.isValid()));
         }
         if (node instanceof ValuedNode<?> valuedNode) {
             if (valuedNode.get() == null) {
-                currentNodeInfo.set("value", NULL_NODE);
+                currentNodeInfo.set("value", NullNode.getInstance());
             } else if (valuedNode instanceof byransha.BooleanNode) {
-                Boolean value = (Boolean) valuedNode.get();
-                currentNodeInfo.set("value", value ? TRUE_NODE : FALSE_NODE);
+                currentNodeInfo.set("value", BooleanNode.valueOf((Boolean) valuedNode.get()));
             } else if (valuedNode instanceof byransha.IntNode) {
-                currentNodeInfo.set("value", nodeFactory.numberNode((Integer) valuedNode.get()));
+                currentNodeInfo.set("value", new IntNode((Integer) valuedNode.get()));
             } else {
-                currentNodeInfo.set("value", nodeFactory.textNode(valuedNode.getAsString()));
+                currentNodeInfo.set("value", new TextNode(valuedNode.getAsString()));
             }
-            currentNodeInfo.set("mimeType", nodeFactory.textNode(valuedNode.getMimeType()));
+            currentNodeInfo.set("mimeType", new TextNode(valuedNode.getMimeType()));
         }
 
         currentNodeInformation.set("currentNode", currentNodeInfo);
 
         node.forEachOut((name, out) -> {
             if(!out.deleted) {
-                var b = nodeFactory.objectNode();
-                b.set("id", nodeFactory.numberNode(out.id()));
-                b.set("name", nodeFactory.textNode(name));
-                b.set("type", nodeFactory.textNode(out.getClass().getSimpleName()));
+                var b = new ObjectNode(null);
+                b.set("id", new IntNode(out.id()));
+                b.set("name", new TextNode(name));
+                b.set("type", new TextNode(out.getClass().getSimpleName()));
 
                 if (out instanceof BusinessNode bn) {
-                    b.set("isValid", bn.isValid() ? TRUE_NODE : FALSE_NODE);
+                    b.set("isValid", BooleanNode.valueOf(bn.isValid()));
                 }
 
                 if (out instanceof ValuedNode<?> vn) {
                     if (vn.get() == null) {
-                        b.set("value", NULL_NODE);
+                        b.set("value", NullNode.getInstance());
                     } else if (vn instanceof byransha.BooleanNode) {
-                        Boolean value = (Boolean) vn.get();
-                        b.set("value", value ? TRUE_NODE : FALSE_NODE);
+                        b.set("value", BooleanNode.valueOf((Boolean) vn.get()));
                     } else if (vn instanceof byransha.IntNode) {
-                        b.set("value", nodeFactory.numberNode((Integer) vn.get()));
+                        b.set("value", new IntNode((Integer) vn.get()));
                     } else {
-                        b.set("value", nodeFactory.textNode(vn.getAsString()));
+                        b.set("value", new TextNode(vn.getAsString()));
                     }
 
-                    b.set("mimeType", nodeFactory.textNode(vn.getMimeType()));
+                    b.set("mimeType", new TextNode(vn.getMimeType()));
                 }
 
                 if (out instanceof ListNode<?> ln) {
-                    b.set("canAddNewNode", ln.canAddNewNode() ? TRUE_NODE : FALSE_NODE);
-                    b.set("isDropdown", ln.isDropdown() ? TRUE_NODE : FALSE_NODE);
+                    b.set("canAddNewNode", BooleanNode.valueOf(ln.canAddNewNode()));
+                    b.set("isDropdown", BooleanNode.valueOf(ln.isDropdown()));
                 } else if (out instanceof SetNode<?> sn) {
-                    b.set("canAddNewNode", sn.canAddNewNode() ? TRUE_NODE : FALSE_NODE);
-                    b.set("isDropdown", sn.isDropdown() ? TRUE_NODE : FALSE_NODE);
+                    b.set("canAddNewNode", BooleanNode.valueOf(sn.canAddNewNode()));
+                    b.set("isDropdown", BooleanNode.valueOf(sn.isDropdown()));
                 } else if (out instanceof DropdownNode<?> dn) {
-                    b.set("canAddNewNode", dn.canAddNewNode() ? TRUE_NODE : FALSE_NODE);
-                    b.set("isDropdown", dn.isDropdown() ? TRUE_NODE : FALSE_NODE);
+                    b.set("canAddNewNode", BooleanNode.valueOf(dn.canAddNewNode()));
+                    b.set("isDropdown", BooleanNode.valueOf(dn.isDropdown()));
                 }
 
-                Field field = findField(node.getClass(), name);
-                
-                if ((out instanceof ListNode<?> || out instanceof SetNode<?> || out instanceof DropdownNode<?>) && field != null) {
+                if (out instanceof ListNode<?> || out instanceof SetNode<?> || out instanceof DropdownNode<?>) {
                     try {
-                        var genericType = field.getGenericType();
-                        if (genericType instanceof ParameterizedType parameterizedType) {
-                            var actualType = parameterizedType.getActualTypeArguments()[0];
-                            b.set("listNodeType", nodeFactory.textNode(actualType.getTypeName()));
+                        Field field = findField(node.getClass(), name);
+                        if (field != null) {
+                            var genericType = field.getGenericType();
+                            if (genericType instanceof ParameterizedType parameterizedType) {
+                                var actualType = parameterizedType.getActualTypeArguments()[0];
+                                b.set("listNodeType", new TextNode(actualType.getTypeName()));
+                            }
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -148,36 +128,36 @@ public class ClassAttributeField extends NodeEndpoint<BNode> implements View {
 
                 if (out instanceof RadioNode<?> radioNode) {
                     try {
-                        ArrayNode optionsArray = nodeFactory.arrayNode();
-                        for (Object option : radioNode.getOptions()) {
-                            optionsArray.add(option == null ? NULL_NODE : nodeFactory.textNode(option.toString()));
-                        }
-                        b.set("options", optionsArray);
+                        b.set("options", JsonNodeFactory.instance.arrayNode().addAll(
+                                radioNode.getOptions().stream()
+                                        .map(option -> option == null ? NullNode.getInstance() : new TextNode(option.toString()))
+                                        .collect(Collectors.toList())));
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
 
+                Field field = findField(node.getClass(), name);
                 if (field != null) {
-                    ObjectNode validations = nodeFactory.objectNode();
+                    var validations = new ObjectNode(null);
                     if (field.isAnnotationPresent(Required.class)) {
-                        validations.set("required", TRUE_NODE);
+                        validations.set("required", BooleanNode.valueOf(true));
                     }
                     if (field.isAnnotationPresent(Min.class)) {
-                        validations.set("min", nodeFactory.numberNode(field.getAnnotation(Min.class).value()));
+                        validations.set("min", new DoubleNode(field.getAnnotation(Min.class).value()));
                     }
                     if (field.isAnnotationPresent(Max.class)) {
-                        validations.set("max", nodeFactory.numberNode(field.getAnnotation(Max.class).value()));
+                        validations.set("max", new DoubleNode(field.getAnnotation(Max.class).value()));
                     }
                     if (field.isAnnotationPresent(Size.class)) {
                         var size = field.getAnnotation(Size.class);
-                        var sizeInfo = nodeFactory.objectNode();
-                        sizeInfo.set("min", nodeFactory.numberNode(size.min()));
-                        sizeInfo.set("max", nodeFactory.numberNode(size.max()));
+                        var sizeInfo = new ObjectNode(null);
+                        sizeInfo.set("min", new IntNode(size.min()));
+                        sizeInfo.set("max", new IntNode(size.max()));
                         validations.set("size", sizeInfo);
                     }
                     if (field.isAnnotationPresent(Pattern.class)) {
-                        validations.set("pattern", nodeFactory.textNode(field.getAnnotation(Pattern.class).regex()));
+                        validations.set("pattern", new TextNode(field.getAnnotation(Pattern.class).regex()));
                     }
                     if (validations.size() > 0) {
                         b.set("validations", validations);
@@ -190,10 +170,7 @@ public class ClassAttributeField extends NodeEndpoint<BNode> implements View {
 
         currentNodeInformation.set("attributes", a);
 
-        StringBuilder idBuilder = new StringBuilder(node.prettyName());
-        idBuilder.append('@').append(node.id());
-        
-        return new EndpointJsonResponse(currentNodeInformation, idBuilder.toString());
+        return new EndpointJsonResponse(currentNodeInformation, node.prettyName()+'@'+node.id());
     }
 
     @Override
