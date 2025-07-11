db761a11302772c446c7b0874cb7f72160fbb19b
TriForMine <quentin@triformine.dev>
2025-07-11 14:50:01 +0200

    Update NodeInfo.java

--- src/main/java/byransha/web/endpoint/NodeInfo.java
+++ src/main/java/byransha/web/endpoint/NodeInfo.java
@@ -1,10 +1,5 @@
 package byransha.web.endpoint;
 
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.TextNode;
-import com.sun.net.httpserver.HttpsExchange;
-
 import byransha.BBGraph;
 import byransha.BNode;
 import byransha.User;
@@ -12,67 +7,71 @@ import byransha.web.EndpointJsonResponse;
 import byransha.web.ErrorResponse;
 import byransha.web.NodeEndpoint;
 import byransha.web.WebServer;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.sun.net.httpserver.HttpsExchange;
 
 public class NodeInfo extends NodeEndpoint<BNode> {
 
-	public NodeInfo(BBGraph db) {
-		super(db);
-	}
+    public NodeInfo(BBGraph db) {
+        super(db);
+    }
 
-	public NodeInfo(BBGraph db, int id) {
-		super(db, id);
-	}
+    public NodeInfo(BBGraph db, int id) {
+        super(db, id);
+    }
 
-	@Override
-	public String whatItDoes() {
-		return "info about a node";
-	}
+    @Override
+    public String whatItDoes() {
+        return "info about a node";
+    }
 
-	@Override
-	public EndpointJsonResponse exec(ObjectNode inputJson, User user, WebServer webServer, HttpsExchange exchange,
-			BNode node) {
-		if (!node.canSee(user)) {
-			return ErrorResponse.forbidden("User does not have permission to view node " + node.id());
-		}
+    @Override
+    public EndpointJsonResponse exec(
+        ObjectNode inputJson,
+        User user,
+        WebServer webServer,
+        HttpsExchange exchange,
+        BNode node
+    ) {
+        if (!node.canSee(user)) {
+            return ErrorResponse.forbidden(
+                "User does not have permission to view node " + node.id()
+            );
+        }
 
-		var r = new ObjectNode(null);
-		r.set("id", new TextNode("" + node.id()));
-		r.set("pretty_name", new TextNode(node.prettyName()));
-		r.set("class", new TextNode(node.getClass().getName()));
-		r.set("to_string", new TextNode(node.toString()));
-		r.set("can read", new TextNode("" + node.canSee(user)));
-		r.set("can write", new TextNode("" + node.canSee(user)));
+        var r = new ObjectNode(null);
+        r.set("id", new TextNode("" + node.id()));
+        r.set("pretty_name", new TextNode(node.prettyName()));
+        r.set("class", new TextNode(node.getClass().getName()));
+        r.set("to_string", new TextNode(node.toString()));
+        r.set("can read", new TextNode("" + node.canSee(user)));
+        r.set("can write", new TextNode("" + node.canSee(user)));
 
-		var outs = new ArrayNode(null);
-		node.forEachOut((name, outNode) -> {
-			if (outNode.canSee(user)) {
-				var out = new ObjectNode(null);
-				out.set(name, new TextNode("" + outNode.id()));
-				outs.add(out);
-			}
-		});
-		r.set("out", outs);
-		var ins = new ArrayNode(null);
-		node.forEachIn((name, inNode) -> {
-			if (inNode.canSee(user)) {
-				var in = new ObjectNode(null);
-				in.set(name, new TextNode("" + inNode.id()));
-				ins.add(in);
-			}
-		});
-		r.set("in", ins);
+        var outs = new ArrayNode(null);
+        node.forEachOut((name, outNode) -> {
+            if (outNode.canSee(user)) {
+                var out = new ObjectNode(null);
+                out.set(name, new TextNode("" + outNode.id()));
+                outs.add(out);
+            }
+        });
+        r.set("out", new IntNode(outs.size()));
+        r.set("in", new IntNode(ins().size()));
 
-		var availableEndpoints = new ArrayNode(null);
+        var availableEndpoints = new ArrayNode(null);
 
-		for (var e : graph.endpointsUsableFrom(node)) {
-			if (e.canSee(user)) {
-				if (e.canExec(user)) {
-					availableEndpoints.add(new TextNode(e.name()));
-				}
-			}
-		}
+        for (var e : graph.endpointsUsableFrom(node)) {
+            if (e.canSee(user)) {
+                if (e.canExec(user)) {
+                    availableEndpoints.add(new TextNode(e.name()));
+                }
+            }
+        }
 
-		r.set("views", availableEndpoints);
-		return new EndpointJsonResponse(r, this);
-	}
+        r.set("views", availableEndpoints);
+        return new EndpointJsonResponse(r, this);
+    }
 }
