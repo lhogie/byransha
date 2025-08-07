6bd50aafc341e08085b237de90c297a89abfd5a4
TriForMine <quentin@triformine.dev>
2025-08-07 10:07:48 +0200

    Remove fieldPath logic and related methods from FilterNode

--- src/main/java/byransha/filter/FilterNode.java
+++ src/main/java/byransha/filter/FilterNode.java
@@ -8,23 +8,12 @@ import java.util.function.Predicate;
 
 public abstract class FilterNode extends BNode {
 
-    public StringNode fieldPath;
-
     protected FilterNode(BBGraph g) {
         super(g);
-        fieldPath = g.create( StringNode.class);
     }
 
     protected FilterNode(BBGraph g, int id) {
         super(g, id);
-    }
-
-    @Override
-    protected void initialized() {
-        super.initialized();
-        if (fieldPath.get() == null) {
-            fieldPath.set("");
-        }
     }
 
     public abstract boolean filter(BNode node);
@@ -44,85 +33,6 @@ public abstract class FilterNode extends BNode {
             .anyMatch(supportedType ->
                 supportedType.isAssignableFrom(nodeClass)
             );
-    }
-
-    protected Object getFieldValue(BNode node) {
-        String path = fieldPath.get();
-        if (path == null || path.trim().isEmpty()) {
-            return node;
-        }
-
-        String[] pathParts = path.split("\\.");
-        Object current = node;
-
-        for (String part : pathParts) {
-            if (current == null) {
-                return null;
-            }
-
-            current = getFieldValueFromObject(current, part);
-        }
-
-        return current;
-    }
-
-    private Object getFieldValueFromObject(Object obj, String fieldName) {
-        if (obj == null) {
-            return null;
-        }
-
-        try {
-            if (obj instanceof BNode bnode) {
-                BNode[] outNodes = new BNode[1];
-                bnode.forEachOut((name, outNode) -> {
-                    if (name.equals(fieldName)) {
-                        outNodes[0] = outNode;
-                    }
-                });
-
-                if (outNodes[0] != null) {
-                    return outNodes[0];
-                }
-            }
-
-            Class<?> clazz = obj.getClass();
-            while (clazz != null) {
-                try {
-                    Field field = clazz.getDeclaredField(fieldName);
-                    field.setAccessible(true);
-                    return field.get(obj);
-                } catch (NoSuchFieldException e) {
-                    clazz = clazz.getSuperclass();
-                }
-            }
-        } catch (Exception e) {
-            System.err.println(
-                "Error accessing field '" +
-                fieldName +
-                "' on object of type " +
-                obj.getClass().getSimpleName() +
-                ": " +
-                e.getMessage()
-            );
-        }
-
-        return null;
-    }
-
-    protected String valueToString(Object value) {
-        if (value == null) {
-            return null;
-        }
-
-        if (value instanceof ValuedNode<?> valuedNode) {
-            return valuedNode.getAsString();
-        }
-
-        if (value instanceof BNode bnode) {
-            return bnode.prettyName();
-        }
-
-        return value.toString();
     }
 
     public Predicate<BNode> toPredicate() {
@@ -148,9 +58,7 @@ public abstract class FilterNode extends BNode {
     }
 
     public void configure(ObjectNode config) {
-        if (config.has("fieldPath")) {
-            fieldPath.set(config.get("fieldPath").asText());
-        }
+        // Base implementation - subclasses can override
     }
 
     public abstract String getFilterDescription();
@@ -165,11 +73,6 @@ public abstract class FilterNode extends BNode {
 
     @Override
     public String prettyName() {
-        String desc = getFilterDescription();
-        String path = fieldPath.get();
-        if (path != null && !path.trim().isEmpty()) {
-            return desc + " on field: " + path;
-        }
-        return desc;
+        return getFilterDescription();
     }
 }
