220b1fec616232151b5ec4a49b66922ea624bd2c
Luc Hogie <luc.hogie@cnrs.fr>
2026-07-27 15:48:16 +0200

    adding files

---
+++ src/main/java/byransha/PrintVersion.java
@@ -0,0 +1,9 @@
+package byransha;
+
+import byransha.nodes.system.Byransha;
+
+public class PrintVersion {
+	public static void main(String[] args) throws Throwable {
+		System.out.println(Byransha.VERSION);
+	}
+}
